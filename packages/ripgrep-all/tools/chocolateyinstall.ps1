e464062f68110d2ea36ea87c371f8fb3c87e0a32
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2019-07-01 21:37:28 +0200

    ripgrep-all 0.9.2

---
+++ packages/ripgrep-all/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$version     = $env:chocolateyPackageVersion
+$packageName = $env:chocolateyPackageName
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url64       = ("https://github.com/phiresky/ripgrep-all/releases/download/{0}/ripgrep_all-{0}-x86_64-pc-windows-msvc.7z" -f $version)
+
+$packageArgs = @{
+    packageName    = $packageName
+    unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+    url64bit       = $url64
+    checksum64     = "50762A940E2552987F04C91D33E87B9E8490616E"
+    checksumType64 = "sha1"
+}
+
+Install-ChocolateyZipPackage @packageArgs
+$File = Get-ChildItem -File -Path $env:ChocolateyInstall\lib\$packageName\tools\ -Filter rga*.exe
+Get-ChocolateyUnzip -fileFullPath $File.FullName -destination $env:ChocolateyInstall\lib\$packageName\tools\
